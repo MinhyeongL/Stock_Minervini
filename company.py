8fb0ad80b99ca583c7d80b9e06e288e15d6c8a34
MinhyeongL <minhyung0123@gmail.com>
2024-03-02 10:42:44 +0900

    Unify Korea and USA

--- company.py
+++ company.py
@@ -14,21 +14,43 @@ from urllib.request import urlopen
 
 
 class Company:
-    def __init__(self, api_key):
-        self.api_key = api_key
+    def __init__(self, market='KRX', magic=False):
+        self.market = market
         self.now = datetime.now().date()
+        self.magic = magic
         
+        
+    def get_company_dict(self):
+        df = fdr.StockListing(self.market)
+        
+        if self.market in ['KRX','KOSPI','KOSDAQ']:
+            df = df[(df.Market == 'KOSPI') | (df.Market == 'KOSDAQ')]
+ 
+            self.company_dict = {row.Name : f"{row.Code}.{'KS' if row.Market == 'KOSPI' else 'KQ'}" \
+                                 for index, row in df.iterrows()} 
+            
+            if self.magic == True:
+                self.get_magic_stock_company_list()
+                self.company_dict = {key: value for key, value in self.company_dict.items() if key in self.magic_list}
     
-    def get_company_list(self, magic=False):
-        stock_list = fdr.StockListing('KRX').dropna().reset_index(drop=True)
-        stock_list = stock_list[(stock_list.Market == 'KOSPI') | (stock_list.Market == 'KOSDAQ')].reset_index(drop = True)
+        elif self.market in ['NASDAQ','SP500','S&P500']:
+
+            self.company_dict = {row.Name : row.Symbol for index, row in df.iterrows()}
+
         
-        if magic:
-            self.get_magic_stock_company_list()
-            stock_list = stock_list[stock_list.Name.isin(self.magic_list)]
-        
-        self.stock_list = stock_list
-        self.stock_code = self.stock_list.Code.tolist()
+    def make_dataset(self, period=2):
+        self.get_company_dict()
+
+        df = pd.DataFrame()
+
+        for name, code in tqdm(self.company_dict.items()):
+            tmp = yf.download(code,
+                              start=self.now-relativedelta(years=period),
+                              end=self.now, progress=False)[['Close']].rename(columns = {'Close':name})
+
+            df = pd.merge(df, tmp, how = 'outer', left_index = True, right_index = True)
+    
+        return df
         
         
     def get_magic_stock_company_list(self):
@@ -45,6 +67,18 @@ class Company:
         for x in source.find_all('td', {'width' : '40%'})[1:]:
             self.magic_list.append(x.get_text().strip())
             
+            
+    def get_company_list(self, magic=False):
+        stock_list = fdr.StockListing('KRX').dropna().reset_index(drop=True)
+        stock_list = stock_list[(stock_list.Market == 'KOSPI') | (stock_list.Market == 'KOSDAQ')].reset_index(drop = True)
+        
+        if magic:
+            self.get_magic_stock_company_list()
+            stock_list = stock_list[stock_list.Name.isin(self.magic_list)]
+        
+        self.stock_list = stock_list
+        self.stock_code = self.stock_list.Code.tolist()
+        
         
     def get_company_info(self, magic=False):
         dart = OpenDartReader(self.api_key)
@@ -67,25 +101,8 @@ class Company:
 
         company_dict = {}
         for x in tqdm(company_list):
+            # 우선주 제외
             if x['status'] == '000':
                 company_dict[x['stock_code']] = (x['stock_name'], x['corp_cls'])
 
         self.company_dict = company_dict
-
-
-    def make_dataset(self, period=3):
-#         self.get_company_info()
-
-        df = pd.DataFrame()
-
-        for code, (name, cls) in tqdm(self.company_dict.items()):
-            if cls == 'Y':
-                market = 'KS'
-            elif cls == 'K':
-                market = 'KQ'
-            tmp = yf.download(f'{code}.{market}',
-                              start = self.now-relativedelta(years=period),
-                              end=self.now, progress=False)[['Close']].rename(columns = {'Close':name})
-            df = pd.merge(df, tmp, how = 'outer', left_index = True, right_index = True)
-        
-        return df
--- minervini.py
+++ minervini.py
@@ -6,6 +6,7 @@ warnings.filterwarnings('ignore')
 import matplotlib.pyplot as plt
 
 from pykrx import stock
+import yfinance as yf
 
 
 class Minervini:
@@ -21,45 +22,85 @@ class Minervini:
             tmp['MA50'] = tmp[col].rolling(50).mean()
             tmp['MA150'] = tmp[col].rolling(150).mean()
             tmp['MA200'] = tmp[col].rolling(200).mean()
-
-
+#             self.tmp = tmp
+            # 1
             if (tmp[col][-1] > tmp['MA150'][-1]) and (tmp[col][-1] > tmp['MA200'][-1]):
                 self.growth_stock.add(col)
             else: continue
-
+            
+            # 2. 150일 이동평균선이 200일 이동평균선 위에 있다.
             if tmp['MA150'][-1] < tmp['MA200'][-1]:
                 self.growth_stock.discard(col)
-
+                
+            # 3. 200일 이동평균선이 적어도 1개월 동안 상승 추세에 있다.
             if tmp['MA200'][-40:].corr(tmp['TimeStamp'][-40:]) < 0.5:
                 self.growth_stock.discard(col)
-
+                
+            # 4. 50(10주)일 이동평균선이 150일 이동평균선 및 200일 이동평균선 위에 있다.
+            if tmp['MA50'][-1] < tmp['MA150'][-1]:
+                self.growth_stock.discard(col)
+            if tmp['MA50'][-1] < tmp['MA200'][-1]:
+                self.growth_stock.discard(col)
+                
+            # 5. 현 주가가 50일 이동평균선 위에 있다.
             if tmp[col][-1] < tmp['MA50'][-1]:
                 self.growth_stock.discard(col)
-
-            if tmp[col][-1] < tmp[col].min()*1.3:
+                
+            # 6. 현 주가가 52주 신저가보다 최소한 30퍼센트 위에 있다.
+            if tmp[col][-1] < tmp.iloc[int(len(tmp)*0.5):, 0].min()*1.3:
                 self.growth_stock.discard(col)
-
-            if tmp[col][-1] < tmp[col].max()*0.75:
+                
+            # 7. 현 주가가 최소한 52주 신고가의 25퍼센트 안에 있다.(신고가에 가까울수록 좋다.)
+            if tmp[col][-1] < tmp.iloc[int(len(tmp)*0.5):, 0].max()*0.75:
                 self.growth_stock.discard(col)
+                    
+    
+#     def filter_eps(self, code_dict, start='20210101', end='20240131'): 
+#         self.growth_finance = dict()
+#         for corp in tqdm(self.growth_stock):
+#             try:
+#                 code = code_dict[corp]
+#                 df = stock.get_market_fundamental_by_date(start, end, code, freq='m')
+#                 df = df.resample('Q').last()
+#                 if df.EPS[1] < df.EPS[5] < df.EPS[9]:
+#                     self.growth_finance[corp] = df
+    
+#             except:
+#                 continue
     
     
-    def filter_eps(self, code_dict, start='20210101', end='20231231'): 
+    def filter_eps(self, code_dict):
         self.growth_finance = dict()
         for corp in tqdm(self.growth_stock):
             try:
                 code = code_dict[corp]
-                df = stock.get_market_fundamental_by_date(start, end, code, freq='m')
-                df = df.resample('Q').last()
-                if df.EPS[1] < df.EPS[5] < df.EPS[9]:
-                    self.growth_finance[corp] = df
-    
+                tmp = yf.Ticker(code)
+                eps = tmp.income_stmt.T[::-1][['Basic EPS']]
+        
+                i = 1
+                sign = False
+                while i < len(eps):
+                    if eps.iloc[i-1, 0] > eps.iloc[i, 0]:
+                        sign = True
+                        break
+                    i += 1
+                if sign == True:
+                    continue
+                self.growth_finance[corp] = eps
             except:
                 continue
-                
+        
+    
+    
     
     def visualize(self):
-        for corp in self.growth_finance.keys():
+        for corp in sorted(list(self.growth_finance.keys())):
             print(corp)
-            plt.figure(figsize=(10, 3))
-            self.company_df[corp].plot()
+            plt.figure(figsize=(10, 6))
+            self.company_df[corp].plot(label = corp)
+            self.company_df[corp].rolling(50).mean().plot(label = 'MA50')
+            self.company_df[corp].rolling(150).mean().plot(label = 'MA150')
+            self.company_df[corp].rolling(200).mean().plot(label = 'MA200')
+            
+            plt.legend(loc = 'lower right')
             plt.show()
